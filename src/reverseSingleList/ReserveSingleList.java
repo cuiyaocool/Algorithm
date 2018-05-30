1048edbeb4065b194d3cc3482e483161bcc8c7ce
cuiyaocool <cuiyaovip@126.com>
2018-05-30 22:56:36 +0800

    单链表反转

--- src/reverseSingleList/ReserveSingleList.java
+++ src/reverseSingleList/ReserveSingleList.java
@@ -1,7 +1,20 @@
 package reverseSingleList;
 
+/**
+ * 通过两种方法完成反转
+ * 1、通过递归
+ * 2、通过遍历
+ * 
+ * 递归是反转当前节点时，先使得后面的结点反转
+ * 
+ * 遍历则是从前向后
+ * 
+ * @author Administrator
+ *
+ */
 public class ReserveSingleList {
 
+	//递归
 	public static ListNode reverseSingleList(ListNode head) {
 		ListNode p = head;
 		if (p == null || p.next == null) {
@@ -12,7 +25,7 @@ public class ReserveSingleList {
 		p.next = null;
 		return backNode;
 	}
-	
+	//遍历
 	public static ListNode reverseSingleList2(ListNode head) {
 		if (head == null) {
 			return head;
@@ -40,19 +53,21 @@ public class ReserveSingleList {
 		l0.next = l1;
 		l1.next = l2;
 		l2.next = l3;
-		
+		//原始值
 		ListNode p = l0;
 		while (p != null) {
 			System.out.print(p.data + "  ");
 			p = p.next;
 		}
 		System.out.println();
+		//递归反转
 		p = reverseSingleList2(l0);
 		while (p != null) {
 			System.out.print(p.data + "  ");
 			p = p.next;
 		}
 		System.out.println();
+		//遍历反转为原始值
 		p = reverseSingleList(l3);
 		while (p != null) {
 			System.out.print(p.data + "  ");
